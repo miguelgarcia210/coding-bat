5b0a22cdb78d8f59f6e707e1eabd78c0a81ccc50
miguelgarcia210 <garcia.a.miguel.210@gmail.com>
2020-10-25 01:52:35 -0500

    Add test cases for method

--- src/warmup_1/SleepIn.java
+++ src/warmup_1/SleepIn.java
@@ -2,9 +2,13 @@ package warmup_1;
 
 public class SleepIn {
     public static void main(String[] args) {
+        System.out.println(sleepIn(false, false));
+        System.out.println(sleepIn(true, false));
+        System.out.println(sleepIn(false, true));
+        System.out.println(sleepIn(true, true));
     }
 
-    public boolean sleepIn(boolean weekday, boolean vacation) {
+    public static boolean sleepIn(boolean weekday, boolean vacation) {
         return !weekday || vacation;
     }
 }
