3647fbf785460cb77795ee69b100b721eeb71106
miguelgarcia210 <garcia.a.miguel.210@gmail.com>
2020-10-25 01:44:02 -0500

    Add project files with function

---
+++ src/warmup_1/SleepIn.java
@@ -0,0 +1,10 @@
+package warmup_1;
+
+public class SleepIn {
+    public static void main(String[] args) {
+    }
+
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        return !weekday || vacation;
+    }
+}
